280ba12545399bda4fb1fc4b08788c8e09ae31fe
MATHEUS_CAVALIERI_XAVIER <22301925@OMEGAF42.FLORESTA>
2024-09-18 14:29:39 -0300

    darkmode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const documento = document.body
+const inputMode = document.querySelector('.input-mode');
+
+inputMode.addEventListener('click', function(e){
+    if (inputMode.checked === true){
+        documento.classList.toggle('dark')
+    }
+    else{
+        documento.classList.remove('dark')
+    }
+})
